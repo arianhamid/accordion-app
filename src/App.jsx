fc7d18da77e40ce3c89ca191902eaf5d32170465
Hamid <hamidarianyegane@gmail.com>
2023-07-19 23:07:46 +0330

    functionality added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
+
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [question, setQuestion] = useState(data);
+  return (
+    <main>
+      <div className="container">
+        <h4>Questions And Answers About Login</h4>
+        <section className="info">
+          {question.map((question) => {
+            return <SingleQuestion key={question.id} question={question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
--- src/Question.jsx
+++ src/Question.jsx
@@ -1,7 +1,19 @@
-import React, { useState } from 'react';
-import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
-const Question = () => {
-  return <h2>question component</h2>;
+import React, { useState } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
+
+const Question = ({ question }) => {
+  const { title, info } = question;
+  const [isShow, setIsShow] = useState(false);
+  return (
+    <article>
+      <h2>{title}</h2>
+      {isShow ? <div>{info}</div> : <p>show content</p>}
+
+      <button className="btn" onClick={() => setIsShow(!isShow)}>
+        {isShow ? "hide" : "show"}
+      </button>
+    </article>
+  );
 };
 
 export default Question;
